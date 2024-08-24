285ca4b9bc96d5ceac8be8a023e8f1d18564f7e2
EnsarDzaf0 <ensardzafo03@gmail.com>
2024-08-24 23:31:37 +0200

    production database adjustment

--- api/config/config.js
+++ api/config/config.js
@@ -7,13 +7,7 @@ module.exports = {
         host: process.env.DB_HOST,
         dialect: 'postgres',
         port: process.env.DB_PORT,
-        /*dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        },
-        logging: console.log*/
+        logging: console.log
     },
     staging: {
         username: process.env.DB_USER,
@@ -24,11 +18,18 @@ module.exports = {
         logging: false
     },
     production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        host: process.env.DB_HOST,
+        username: process.env.DB_USER_PROD,
+        password: process.env.DB_PASSWORD_PROD,
+        database: process.env.POSTGRES_DB_PROD,
+        host: process.env.DB_HOST_PROD,
+        port: process.env.DB_PORT_PROD,
         dialect: 'postgres',
-        logging: false
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
     }
 };
