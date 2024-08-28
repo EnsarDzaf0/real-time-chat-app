2f9f48eb575bbe633cf030ddbd88b15c039858c6
EnsarDzaf0 <ensardzafo03@gmail.com>
2024-08-28 21:21:43 +0200

    Socket to live BE url

--- app/src/utils/socket.ts
+++ app/src/utils/socket.ts
@@ -1,6 +1,5 @@
 import io from 'socket.io-client';
-import { baseUrl } from '../services/services';
 
-const socket = io('ws://localhost:8080');
+const socket = io('wss://zamger.site');
 
 export default socket;
